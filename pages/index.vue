eebc734af9d7216d50994c77a1b3dc0e9d91ae38
مجید نوری <0065964330@funds.local>
2025-03-11 16:45:07 +0330

    Upload simple PWA project on Github

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class=" px-[150px] p-4 rounded-sm">
+    <form >
+      <input
+        type="text"
+        placeholder="Search for characters"
+        class="w-[1462px] h-[48px] rounded-sm bg-dark-3 outline-none text-white gap-4 mr-4 pl-4"
+      />
+      <button class="bg-btn-red w-[98px] h-[48px] rounded-sm text-white">
+        Search
+      </button>
+    </form>
+
+    <div class="grid grid-cols-4 gap-5 bg-dark-2 py-5">
+        <div class=" h-[542px]" >
+          <img width="100%" height="100%" src="/img/2222.jpg" alt="">
+        </div>
+        <div class="w-100 h-[542px]"><img src="/img/11111.jpg" alt=""></div>
+        <div>eee</div>
+        <div>eeee</div>
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
