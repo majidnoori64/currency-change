ed886ad9a9cfce3ad525034313c762d5758c5f12
مجید نوری <0065964330@funds.local>
2025-03-12 15:48:28 +0330

    test

---
+++ layouts/default/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="h-auto pb-5 bg-dark-1 md:px-[150px] sm:px-4">
+      <div class="mb-[40px] pt-3">
+        <button class="bg-btn-red px-3 py-2 rounded-sm text-white">logo</button>
+      </div>
+      <form>
+        <div class="bg-dark-2 p-4 rounded-sm w-[100%]">
+          <input
+            type="text"
+            placeholder="Search for characters"
+            class="md:w-[92.7%] sm:w-[80%] h-[48px] rounded-sm bg-dark-3 outline-none text-white gap-4 mr-4 pl-4"
+          />
+          <button class="bg-btn-red md:w-[98px]  h-[48px] rounded-sm text-white">
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+    <div class="px-[150px] pt-[64px]">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,86 @@
 <template>
-  <div class=" px-[150px] p-4 rounded-sm">
-    <form >
-      <input
-        type="text"
-        placeholder="Search for characters"
-        class="w-[1462px] h-[48px] rounded-sm bg-dark-3 outline-none text-white gap-4 mr-4 pl-4"
-      />
-      <button class="bg-btn-red w-[98px] h-[48px] rounded-sm text-white">
-        Search
+  <div>
+    
+
+    <div class="grid grid-cols-4 gap-5 bg-dark-2 ">
+      <div class="h-[542px]">
+        <img
+          width="100%"
+          height="100%"
+          src="/img/2222.jpg"
+          class="object-cover h-full"
+          alt=""
+        />
+        <span class="mt-4 text-white text-base">title</span>
+      </div>
+      <div class="h-[542px]">
+        <img
+          width="100%"
+          height="100%"
+          src="/img/2222.jpg"
+          class="object-cover h-full"
+          alt=""
+        />
+        <span class="mt-4 text-white text-base">title</span>
+      </div>
+      <div class="h-[542px]">
+        <img
+          width="100%"
+          height="100%"
+          src="/img/11111.jpg"
+          class="object-cover h-full"
+          alt=""
+        />
+        <span class="text-white text-base">title</span>
+      </div>
+      <div class="h-[542px]">
+        <img
+          width="100%"
+          height="100%"
+          src="/img/11111.jpg"
+          class="object-cover h-full"
+          alt=""
+        />
+        <span class="text-white text-base">title</span>
+      </div>
+    </div>
+
+    <div class="flex space-x-1 mt-15 justify-center">
+      <button
+        class="rounded-full py-2 text-center text-sm transition-all shadow-sm hover:shadow-lg text-white hover:text-white focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+      >
+        <<
       </button>
-    </form>
-
-    <div class="grid grid-cols-4 gap-5 bg-dark-2 py-5">
-        <div class=" h-[542px]" >
-          <img width="100%" height="100%" src="/img/2222.jpg" alt="">
-        </div>
-        <div class="w-100 h-[542px]"><img src="/img/11111.jpg" alt=""></div>
-        <div>eee</div>
-        <div>eeee</div>
-
+      <button
+        class="rounded-full py-2 px-3 text-center text-sm transition-all shadow-sm hover:shadow-lg text-white hover:text-white focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+      >
+        <
+      </button>
+      <button
+        class="min-w-9 rounded-full bg-red-500 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-red-500 hover:bg-red-500 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+      >
+        1
+      </button>
+      <button
+        class="min-w-9 rounded-full border border-slate-300 py-1 px-2.5 text-center text-sm transition-all shadow-sm hover:shadow-lg text-white hover:text-white hover:bg-red-500 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+      >
+        2
+      </button>
+      <button
+        class="min-w-9 rounded-full border border-slate-300 py-1 px-2.5 text-center text-sm transition-all shadow-sm hover:shadow-lg text-white hover:text-white hover:bg-red-500 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+      >
+        3
+      </button>
+      <button
+        class="min-w-9 rounded-full py-1 text-center text-sm transition-all shadow-sm hover:shadow-lg text-white hover:text-white focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+      >
+        >
+      </button>
+      <button
+        class="min-w-9 rounded-full py-1 text-center text-sm transition-all shadow-sm hover:shadow-lg text-white hover:text-white focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+      >
+        >>
+      </button>
     </div>
   </div>
 </template>
